24f2481c44f11832afc17a9b5b81dbadaeb5b654
vikas <vikash75@gmail.com>
2021-05-31 17:45:09 +0530

    TextscrollingApp

---
+++ app/src/main/java/com/vk_in/textandscrollingviews/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vk_in.textandscrollingviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
